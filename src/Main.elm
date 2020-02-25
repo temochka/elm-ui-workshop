d4339f6f3107e9a9bb5812a88bb8b0b192862543
Artem Chistyakov <chistyakov.artem@gmail.com>
2020-02-24 23:07:25 -0500

    Add layout.

--- src/Main.elm
+++ src/Main.elm
@@ -13,9 +13,21 @@ type alias Model =
     ()
 
 
+layout : { sidebar : Element Msg, header : Element Msg, chat : Element Msg, editor : Element Msg } -> Element Msg
+layout { sidebar, header, chat, editor } =
+    Element.row [ Element.width Element.fill, Element.height Element.fill, Element.explain Debug.todo ]
+        [ Element.el [ Element.width (Element.px 220), Element.height Element.fill, Element.explain Debug.todo ] sidebar
+        , Element.column [ Element.width Element.fill, Element.height Element.fill, Element.explain Debug.todo ]
+            [ Element.el [ Element.width Element.fill, Element.height (Element.px 70) ] header
+            , Element.el [ Element.width Element.fill, Element.height Element.fill ] chat
+            , Element.el [ Element.width Element.fill, Element.height (Element.px 70) ] editor
+            ]
+        ]
+
+
 view : Model -> Html Msg
 view _ =
-    Element.layout [] (Element.paragraph [] [ Element.text "Basic Elm app" ])
+    Element.layout [] (layout { sidebar = Element.none, header = Element.none, chat = Element.none, editor = Element.none })
 
 
 update : Msg -> Model -> Model
