33a2556524988c59471c5c650991015d606f83c6
Artem Chistyakov <chistyakov.artem@gmail.com>
2020-02-24 23:19:15 -0500

    Color sidebar.

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Element exposing (Element)
+import Element.Background
+import Element.Font
 import Html exposing (Html)
 
 
@@ -13,14 +15,43 @@ type alias Model =
     ()
 
 
+sidebarBgColor : Element.Color
+sidebarBgColor =
+    Element.rgb255 48 7 49
+
+
+sidebarFontColor : Element.Color
+sidebarFontColor =
+    Element.rgb255 195 179 195
+
+
 layout : { sidebar : Element Msg, header : Element Msg, chat : Element Msg, editor : Element Msg } -> Element Msg
-layout { sidebar, header, chat, editor } =
+layout content =
+    let
+        sidebar =
+            Element.el
+                [ Element.width (Element.px 220)
+                , Element.height Element.fill
+                , Element.Background.color sidebarBgColor
+                , Element.Font.color sidebarFontColor
+                ]
+                content.sidebar
+
+        header =
+            Element.el [ Element.width Element.fill, Element.height (Element.px 70) ] content.header
+
+        chat =
+            Element.el [ Element.width Element.fill, Element.height Element.fill ] content.chat
+
+        editor =
+            Element.el [ Element.width Element.fill, Element.height (Element.px 70) ] content.editor
+    in
     Element.row [ Element.width Element.fill, Element.height Element.fill, Element.explain Debug.todo ]
-        [ Element.el [ Element.width (Element.px 220), Element.height Element.fill, Element.explain Debug.todo ] sidebar
+        [ sidebar
         , Element.column [ Element.width Element.fill, Element.height Element.fill, Element.explain Debug.todo ]
-            [ Element.el [ Element.width Element.fill, Element.height (Element.px 70) ] header
-            , Element.el [ Element.width Element.fill, Element.height Element.fill ] chat
-            , Element.el [ Element.width Element.fill, Element.height (Element.px 70) ] editor
+            [ header
+            , chat
+            , editor
             ]
         ]
 
